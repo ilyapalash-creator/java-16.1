b5be56929b2460205a4a63353834dc1ced0eaee8
Ilya Palash <rudut.ilya@ya.ru>
2026-06-15 20:47:44 +0300

    Fallen Construction

---
+++ src/test/java/PostmanTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanTest {
+    @Test
+    void shouldEchoSentData() {
+        String dataToSend = "some data";
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body(dataToSend)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("wrong"));
+    }
+}
